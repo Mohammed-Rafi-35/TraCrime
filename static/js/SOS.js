a46884a6adc07b21dfb9041860521719e8a86ef7
Santhosh. S <2005.santhoshsivakumar@gmail.com>
2024-06-11 13:54:23 +0530

    changed files

--- static/js/SOS.js
+++ static/js/SOS.js
@@ -6,21 +6,4 @@ SOS_btn.addEventListener('click', () => {
     setTimeout(() => {
         alert('SOS Alerted! Be Safe');
     }, 1000)
-
-    while(1) {
-        count += 1;
-        setTimeout(()=> {
-            SOS_btn.style.transitionm = 'all 1s ease';
-            SOS_btn.style.boxShadow = '0px 0px 0px red;';
-        }, 10000)
-        setTimeout(()=> {
-            SOS_btn.style.transitionm = 'all 1s ease';
-            SOS_btn.style.boxShadow = '0px 0px 200px red;';
-        }, 10000)
-        if(count >= 10){ 
-            console.log('Hello');
-            break;
-           
-        }
-    }
 });
