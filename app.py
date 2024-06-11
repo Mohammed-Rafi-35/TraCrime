ba2c3a2be78bb1b3e7338141668425da592854da
Santhosh. S <2005.santhoshsivakumar@gmail.com>
2024-06-11 10:21:25 +0530

    modified files

--- app.py
+++ app.py
@@ -1,10 +1,10 @@
 from flask import Flask, render_template
 
-app = flask(__name__)
+app = Flask(__name__)
 
 @app.route('/')
 def home_html():
-    return render_template('home.html')
+    return render_template('home.html', title = 'Home')
 
 if __name__ == '__main__':
     app.run(debug=True)
