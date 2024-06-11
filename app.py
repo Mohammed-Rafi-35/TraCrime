c021b8ca615643741b789f74c6429e5b9619faa8
rafi <mohammedrafi1254>
2024-06-11 10:11:28 +0530

    added home.html modified app.py

--- app.py
+++ app.py
@@ -1,2 +1,10 @@
 from flask import Flask, render_template
 
+app = flask(__name__)
+
+@app.route('/')
+def home_html():
+    return render_template('home.html')
+
+if __name__ == '__main__':
+    app.run(debug=True)
